f9b6909a0ba66a0a225ceb193e345a5407073612
PLucasDev <plucas.soares01@gmail.com>
2024-12-03 12:49:41 -0300

    versão 2 - adição do menu drawer

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/login";
+import SwipeableTemporaryDrawer from "./pages/Teste/menuDrawer"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ListarTarefa />
+      < ListarTarefa/>
     </div>
   );
 }
